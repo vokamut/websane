8a026d6572e3488241292ef47459a26a8900829d
vokamut <vokamut@gmail.com>
2022-05-23 14:22:54 +0300

    Add cropperjs

--- download_latest_static.sh
+++ download_latest_static.sh
@@ -6,7 +6,9 @@ mkdir -p webfonts
 
 wget https://cdn.jsdelivr.net/npm/vue@2 -O js/vue.min.js
 wget https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js -O js/axios.min.js
-wget https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js -O js/bootstrap.bundle.min.js
+wget https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js -O js/bootstrap.min.js
+wget https://cdn.jsdelivr.net/npm/cropperjs/dist/cropper.min.js -O js/cropperjs.min.js
 
 wget https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css -O css/bootstrap.min.css
 wget https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css -O css/fontawesome.min.css
+wget https://cdn.jsdelivr.net/npm/cropperjs/dist/cropper.min.css -O css/cropperjs.min.css

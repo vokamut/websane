ad6b81325cb6224726339a787fc6013fddd01b98
vokamut <vokamut@gmail.com>
2022-06-08 13:59:49 +0300

    Remove bootstrap.min.js

--- download_latest_static.sh
+++ download_latest_static.sh
@@ -6,7 +6,6 @@ mkdir -p webfonts
 
 wget https://cdn.jsdelivr.net/npm/vue@2 -O js/vue.min.js
 wget https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js -O js/axios.min.js
-wget https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js -O js/bootstrap.min.js
 wget https://cdn.jsdelivr.net/npm/cropperjs/dist/cropper.min.js -O js/cropperjs.min.js
 
 wget https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css -O css/bootstrap.min.css
